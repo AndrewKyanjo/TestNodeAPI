a9b081771a37775cc9776b60d4e1a8585146f18e
Andrew Kyanjo <kyanjoa5@gmail.com>
2026-02-12 14:25:18 +0300

    Add endpoint to filter destinations by continent
    Introduced a new GET endpoint '/api/continents/{continent}' to filter destinations based on continent. Also fixed an incorrect import path for the 'errors' module.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import http from 'node:http';
 import {getDataFromDB} from './database/db.js';
-import {errors} from 'errors/error.js'
+import {errors} from './errors/error.js'
 
 const PORT = 3000;
 
@@ -13,14 +13,20 @@ http
             res.setHeader('Content-Type', 'application/json')
             res.statusCode = 200;
             res.end(JSON.stringify(destinations))
-        }
-        else {
+        } else if (req.url.startsWith('/api/continents') && req.method === 'GET') {
+            const continent = req.url.split('/').pop().toLowerCase()
+            const filteredDestinations = destinations.filter(destination => {
+                return destination.continent.toLowerCase() === continent
+            })
+            res.setHeader('Content-Type', 'application/json')
+            res.statusCode = 200;
+            res.end(JSON.stringify(filteredDestinations))
+        } else {
             res.statusCode = errors.notFound.statusCode;
             res.end(errors.notFound.message)
         }
     })
     .listen(PORT, () => {
-        console.log(
-            `Server running on port ${PORT}
+        console.log(`Server running on port ${PORT}
         http://localhost:${PORT}`)
     })

7ceb09fe71bcb1a7bed6ddd6e8bf03a0dc3dde52
Andrew Kyanjo <kyanjoa5@gmail.com>
2026-02-13 16:04:39 +0300

    Use `writeHead` for setting response headers.
    Replaced `statusCode` and `setHeader` with a single `writeHead` call to simplify and modernize the HTTP response setup. This improves code clarity and adheres to standard practices.

--- API 2/server.js
+++ API 2/server.js
@@ -4,8 +4,12 @@ const PORT = 8000;
 
 http
     .createServer((request, response) => {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/html')
+        //response.statusCode = 200
+        //response.setHeader('Content-Type', 'text/html')
+        //New way of doing it
+        response.writeHead(
+            200,
+            {'Content-Type': 'text/html'})
         response.end(`<html><h1>Hello World</h1></html>`)
     })
     .listen(PORT, () => {

ebd4bcb58eb78785d638164bea9a3fb52955c220
Andrew Kyanjo <kyanjoa5@gmail.com>
2026-02-13 16:03:58 +0300

    Add basic HTTP server to API 2
    Created a simple HTTP server in API 2 using Node.js. The server listens on port 8000 and responds with a "Hello World" HTML message. This serves as the boilerplate setup for future API development.

---
+++ API 2/server.js
@@ -0,0 +1,13 @@
+import http from 'node:http';
+
+const PORT = 8000;
+
+http
+    .createServer((request, response) => {
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/html')
+        response.end(`<html><h1>Hello World</h1></html>`)
+    })
+    .listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`)
+    })
